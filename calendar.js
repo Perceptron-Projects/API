2f6cbcf42b0c6a0ed22ef1edb9b79b20b843c3b4
Madhura Jayashanka <madhura@Madhuras-MacBook-Air.local>
2024-01-02 10:24:27 +0530

    Finalizing the code base till interim

--- calendar.js
+++ calendar.js
@@ -160,9 +160,43 @@ app.get("/api/calendar/holidays", rolesMiddleware(["admin","hr","employee"]), as
       console.log(error);
       res.status(500).json({ error: "Could not retrieve all holidays" });
   }
-}
+});
         
-app.get("/api/calendar/leaves/:day/:employeeId", rolesMiddleware(["admin","hr","employee"]), async function (req, res) {}
+app.get("/api/calendar/leaves/:day/:employeeId", rolesMiddleware(["admin","hr","employee"]), async function (req, res) {
+  try {
+    const day = req.params.day;
+    const employeeId = req.params.employeeId;
+
+    if (!isValidDate(day)) {
+      return res.status(400).json({ error: 'Invalid date format for "day"' });
+    } else if (typeof employeeId !== "string") {
+      return res.status(400).json({ error: '"employeeId" must be a string' });
+    }
+
+    const params = {
+      TableName: LEAVES_CALENDAR_TABLE,
+      Key: {
+        Day: day,
+        EmployeeId: employeeId,
+      },
+    };
+
+    const { Item } = await dynamoDbClient.send(new GetCommand(params));
+
+    if (Item) {
+      res.json(Item);
+    } else {
+      res
+        .status(404)
+        .json({
+          error: "Could not find leave for the provided day and employeeId",
+        });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Could not retrieve leave" });
+  }
+});
         
 app.get("/api/calendar/leaves/all", rolesMiddleware(["admin","hr"]), async function (req, res) {
    try {
@@ -218,7 +252,7 @@ app.post("/api/calendar/leaves", rolesMiddleware(["admin","hr","employee"]), asy
     console.error(error);
     res.status(500).json({ error: "Could not create leave" });
   }
-}
+});
 
 
 
