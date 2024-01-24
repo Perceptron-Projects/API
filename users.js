944fbcdc92c86c033dfd478c49d1214a904c8c0c
Madhura Jayashanka <madhura@Madhuras-MacBook-Air.local>
2024-01-24 09:17:10 +0530

    image uploading added

--- users.js
+++ users.js
@@ -15,19 +15,23 @@ const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./middlewares/authMiddleware");
 const { rolesMiddleware } = require("./middlewares/rolesMiddleware");
 const errors = require('./errors');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
 
 const app = express();
 
 const ADMINS_TABLE = process.env.ADMINS_TABLE;
 const EMPLOYEES_TABLE = process.env.EMPLOYEES_TABLE;
 const COMPANY_TABLE = process.env.COMPANY_TABLE;
-
+const IMAGES_BUCKET_NAME = process.env.IMAGES_BUCKET_NAME;
 const JWT_SECRET = process.env.JWT_SECRET; 
+
+const s3Client = new S3Client();
 
 const client = new DynamoDBClient();
 const dynamoDbClient = DynamoDBDocumentClient.from(client);
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 
 app.use((req, res, next) => {
@@ -267,6 +271,46 @@ app.get("/api/users/hr/all", rolesMiddleware(["admin"]), async function (req, re
     res.status(500).json({ error: errors.getHRPersonsError });
   }});
 
+app.get("/api/users/admins/all", rolesMiddleware(["superadmin"]), async function (req, res) {
+  try {
+    // Retrieve all admins
+    const adminParams = {
+      TableName: ADMINS_TABLE,
+      FilterExpression: "#role = :role",
+      ExpressionAttributeNames: {
+        "#role": "role",
+      },
+      ExpressionAttributeValues: {
+        ":role": "admin",
+      },
+    };
+
+    const { Items: adminPersons } = await dynamoDbClient.send(new ScanCommand(adminParams));
+    res.json(adminPersons);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: errors.getAdminPersonsError });
+  }
+});
+
+app.get("/api/users/companies/all", rolesMiddleware(["superadmin"]), async function (req, res) {
+  
+
+  try {
+    // Retrieve all companies
+    const params = {
+      TableName: COMPANY_TABLE,
+    };
+
+    const { Items: companies } = await dynamoDbClient.send(new ScanCommand(params));
+
+    res.json(companies);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: errors.getCompaniesError });
+  }
+});
+
 app.post("/api/users/create-user", rolesMiddleware(["admin"]), async function (req, res) {
    const { name, email, password, role, username, contactNo, birthday, joinday, permissions } = req.body;
 
@@ -286,11 +330,10 @@ app.post("/api/users/create-user", rolesMiddleware(["admin"]), async function (r
     return;
   }
 
-  const adminUserId = req.user.userId; // Assuming you have the authenticated user info in req.user
-
+  const adminUserId = req.user.userId; 
   // Retrieve the admin's company information from the database
   const adminParams = {
-    TableName: ADMINS_TABLE, // Change this table name accordingly
+    TableName: ADMINS_TABLE,  
     Key: {
       userId: adminUserId,
     },
@@ -308,7 +351,7 @@ app.post("/api/users/create-user", rolesMiddleware(["admin"]), async function (r
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const params = {
-      TableName: EMPLOYEES_TABLE, // Assuming EMPLOYEES_TABLE is used for HR users
+      TableName: EMPLOYEES_TABLE, 
       Item: {
         userId: userId,
         name: name,
@@ -343,66 +386,96 @@ app.post("/api/users/create-user", rolesMiddleware(["admin"]), async function (r
   }
 });
 
-
 app.post("/api/users/company/create", rolesMiddleware(["superadmin"]), async function (req, res) {
-   const { companyName, companyLocation, companyEmail } = req.body;
+  const { companyName, companyLocation, companyEmail, contactNo } = req.body;
 
-  // Validate input data
-  if (
-    typeof companyName !== "string" ||
-    typeof companyLocation !== "string" ||
-    typeof companyEmail !== "string"
-  ) {
-    res.status(400).json({ error: errors.invalidInputData });
-    return;
-  }
+ // Validate input data
+ if (
+   typeof companyName !== "string" ||
+   typeof companyLocation !== "string" ||
+   typeof companyEmail !== "string"||
+   typeof contactNo !== "string"
+ ) {
+   res.status(400).json({ error: errors.invalidInputData });
+   return;
+ }
 
-  const companyId = uuidv4(); // Generate a unique companyId
+ let imageUrl = '';
+ if(req.body.image){
+   try {
+     // Await the result of the uploadImage function
+     const uploadResult = await uploadImage(req.body.image);
+     imageUrl = uploadResult.imageUrl;
+   } catch (error) {
+     console.error("Error:", error);
+     res.status(500).json({ error: "Image upload failed" });
+     return;
+   }
+ }
 
-  const params = {
-    TableName: COMPANY_TABLE,
-    Item: {
-      companyId: companyId,
-      companyName: companyName,
-      companyLocation: companyLocation,
-      companyEmail: companyEmail,
-    },
-  };
+ const companyId = uuidv4(); // Generate a unique companyId
 
-  try {
-    await dynamoDbClient.send(new PutCommand(params));
-    res.json({ companyId, companyName, companyLocation, companyEmail });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: errors.createCompanyError });
-  }
+ const params = {
+   TableName: COMPANY_TABLE,
+   Item: {
+     companyId: companyId,
+     companyName: companyName,
+     companyLocation: companyLocation,
+     companyEmail: companyEmail,
+     contactNo: contactNo,
+     companyImageUrl: imageUrl,
+   },
+ };
+
+ try {
+   await dynamoDbClient.send(new PutCommand(params));
+   res.json({ companyId, companyName, companyLocation, companyEmail, contactNo, companyImageUrl: imageUrl });
+ } catch (error) {
+   console.error(error);
+   res.status(500).json({ error: errors.createCompanyError });
+ }
 });
 
 app.post("/api/users/create-admin", rolesMiddleware(["superadmin"]), async function (req, res) {
+  try {
     const { name, email, contactNo, username, password, companyId } = req.body;
 
-  // Validate input data
-  if (
-    typeof name !== "string" ||
-    typeof email !== "string" ||
-    (typeof contactNo !== "string" && typeof contactNo !== "number") ||
-    typeof username !== "string" ||
-    (typeof password !== "string" && typeof password !== "number") ||
-    typeof companyId !== "string"
-  ) {
-    res.status(400).json({ error: errors.invalidInputData });
-    return;
-  }
+    // Validate input data
+    if (
+      typeof name !== "string" ||
+      typeof email !== "string" ||
+      (typeof contactNo !== "string" && typeof contactNo !== "number") ||
+      typeof username !== "string" ||
+      (typeof password !== "string" && typeof password !== "number") ||
+      typeof companyId !== "string"
+    ) {
+      res.status(400).json({ error: errors.invalidInputData });
+      return;
+    }
 
-  // Check if the companyId exists in COMPANY_TABLE
-  const companyParams = {
-    TableName: COMPANY_TABLE,
-    Key: {
-      companyId: companyId,
-    },
-  };
+    let imageUrl = '';
 
-  try {
+    if (req.body.image) {
+      try {
+        // Await the result of the uploadImage function
+        const uploadResult = await uploadImage(req.body.image);
+        console.log(uploadResult);
+        imageUrl = uploadResult.imageUrl;
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Image upload failed" });
+        return;
+      }
+    }
+
+    // Check if the companyId exists in COMPANY_TABLE
+    const companyParams = {
+      TableName: COMPANY_TABLE,
+      Key: {
+        companyId: companyId,
+      },
+    };
+
     const { Item: company } = await dynamoDbClient.send(new GetCommand(companyParams));
 
     if (!company) {
@@ -426,11 +499,12 @@ app.post("/api/users/create-admin", rolesMiddleware(["superadmin"]), async funct
         role: "admin",
         companyId: companyId,
         companyName: company.companyName, // Include the company name
+        adminImageUrl: imageUrl,
       },
     };
 
     await dynamoDbClient.send(new PutCommand(params));
-    res.json({ userId, name, email, contactNo, username, companyId, companyName: company.companyName, role: "admin" });
+    res.json({ userId, name, email, contactNo, username, companyId, companyName: company.companyName, role: "admin", adminImageUrl: imageUrl });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: errors.createUserError });
@@ -491,6 +565,34 @@ app.post("/api/users/login", async function (req, res) {
   }
 });
 
+async function uploadImage(imageDataUri) {
+  try {
+    // Extract base64-encoded image data from the data URL
+    const base64Image = imageDataUri.split(',')[1];
 
+    // Decode base64 string to Buffer
+    const decodedImage = Buffer.from(base64Image, 'base64');
+
+    const imageId = uuidv4();
+
+    const uploadParams = {
+      Bucket: IMAGES_BUCKET_NAME,
+      Key: `${imageId}.jpg`,
+      Body: decodedImage,
+      ContentType: "image/jpg",
+      ACL: "public-read",
+    };
+
+   
+
+    await s3Client.send(new PutObjectCommand(uploadParams));
+
+    const imageUrl = `https://${IMAGES_BUCKET_NAME}.s3.amazonaws.com/${imageId}.jpg`;
+    return { imageUrl };
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Image upload failed");
+  }
+}
 
 module.exports.handler = serverless(app);
