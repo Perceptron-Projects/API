90aa71b9296380f8de450994029ab3cdeb744c49
Madhura Jayashanka <madhuraweerasooriye@gmail.com>
2024-06-15 20:11:40 +0530

    removed index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const router = express.Router();
-
-
-module.exports.handler = serverless(app);
