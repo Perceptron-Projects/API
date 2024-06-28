79d8da083f2a8e554ecb6eeec8976efac80f97d3
KasunJayasanka <officialblackknightcoders@gmail.com>
2024-06-28 08:57:12 +0530

    Syntax Resloved

--- users.js
+++ users.js
@@ -47,8 +47,8 @@ app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 
+
 app.use((req, res, next) => {
-  app.use((req, res, next) => {
     const openPaths = [
       "/api/users/login",
       "/api/users/forget-password",
